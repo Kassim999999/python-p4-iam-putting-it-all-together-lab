c2418c71e3f363810785142d035bbcfabf182514
Kassim <kassimrooney@gmail.com>
2025-06-22 13:30:59 +0300

    Completed lab

--- server/models.py
+++ server/models.py
@@ -7,9 +7,56 @@ from config import db, bcrypt
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
 
-    pass
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True, nullable=False)
+    _password_hash = db.Column(db.String, nullable=True)
+    image_url = db.Column(db.String)
+    bio = db.Column(db.String)
+
+    recipes = db.relationship('Recipe', backref='user', lazy=True)
+
+    serialize_rules = ('-recipes.user', '-_password_hash')
+
+    @hybrid_property
+    def password_hash(self):
+        raise AttributeError("Password hashes may not be viewed.")
+
+    @password_hash.setter
+    def password_hash(self, password):
+        if password:
+            self._password_hash = bcrypt.generate_password_hash(password.encode('utf-8')).decode('utf-8')
+        else:
+            raise ValueError("Password is required.")
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(self._password_hash, password.encode('utf-8'))
+
+    @validates('username')
+    def validate_username(self, key, value):
+        if not value:
+            raise ValueError("Username is required.")
+        return value
+
 
 class Recipe(db.Model, SerializerMixin):
     __tablename__ = 'recipes'
-    
-    pass
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    instructions = db.Column(db.String, nullable=False)
+    minutes_to_complete = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    serialize_rules = ('-user.recipes',)
+
+    @validates('title')
+    def validate_title(self, key, value):
+        if not value:
+            raise ValueError("Title is required.")
+        return value
+
+    @validates('instructions')
+    def validate_instructions(self, key, value):
+        if not value or len(value) < 50:
+            raise ValueError("Instructions must be at least 50 characters long.")
+        return value
